81a65451552f5057bcb4df3d9c39fa8ee99c1702
Alex Rako <147698888+Rako75@users.noreply.github.com>
2025-03-18 10:00:08 +0100

    Update Chelsea_app.py

--- Chelsea_app.py
+++ Chelsea_app.py
@@ -2,11 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 
-# Charger les données CSV et Lecture avec le format de date correct
-gps_data = pd.read_csv('CFC GPS Data.csv', encoding='ISO-8859-1')
+# Charger les données CSV 
+# Lecture avec le format de date correct
+gps_data = pd.read_csv("CFC GPS Data.csv", encoding='ISO-8859-1', parse_dates=['date'], dayfirst=True)
 physical_capability_data = pd.read_csv("CFC Physical Capability Data_.csv", encoding='ISO-8859-1', parse_dates=['date'], dayfirst=True)
 recovery_status_data = pd.read_csv("CFC Recovery status Data.csv", encoding='ISO-8859-1', parse_dates=['date'], dayfirst=True)
 priority_areas_data = pd.read_csv("CFC Individual Priority Areas.csv", encoding='ISO-8859-1', parse_dates=['date'], dayfirst=True)
+
 
 # Titre de l'application
 st.title("Suivi de la Performance Physique et Récupération du Joueur")
